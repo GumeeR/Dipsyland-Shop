a2935871686252a47adbc9750f0eb77094d7695f
GumeeR <jksortealo56@gmail.com>
2025-02-14 01:56:04 -0500

    .

---
+++ src/controllers/payment.controller.js
@@ -0,0 +1,50 @@
+import mercadopage from "mercadopago";
+import { MERCADOPAGO_API_KEY } from "../js/config.js";
+
+export const createOrder = async (req, res) => {
+  mercadopage.configure({
+    access_token: MERCADOPAGO_API_KEY,
+  });
+
+  try {
+    const result = await mercadopage.preferences.create({
+      items: [
+        {
+          title: "Laptop",
+          unit_price: 500,
+          currency_id: "PEN",
+          quantity: 1,
+        },
+      ],
+      notification_url: "https://48ce-186-86-110-36.ngrok-free.app/webhook",
+      back_urls: {
+        success: "http://localhost:3000/success",
+        pending: "http://localhost:3000/pending",
+        failure: "http://localhost:3000/failure",
+      },
+    });
+
+    console.log(result);
+
+    // res.json({ message: "Payment creted" });
+    res.json(result.body);
+  } catch (error) {
+    return res.status(500).json({ message: "Something goes wrong" });
+  }
+};
+
+export const receiveWebhook = async (req, res) => {
+  try {
+    const payment = req.query;
+    console.log(payment);
+    if (payment.type === "payment") {
+      const data = await mercadopage.payment.findById(payment["data.id"]);
+      console.log(data);
+    }
+
+    res.sendStatus(204);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something goes wrong" });
+  }
+};
